6b07dd95295d813860b012aecbfbd2e84581de8d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-12-16 17:16:44 +0200

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\CarController;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +12,32 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::middleware('auth')->group(function (){
+    Route::get('car/search',[CarController::class,'search'])
+        ->name('car.search');
+
+    Route::get('car/watchlist',[CarController::class,'watchlist'])
+        ->name('car.watchlist');
+
+    Route::get('car/index',[CarController::class,'index'])
+        ->name('car.index');
+
+    Route::get('car/create',[CarController::class,'create'])
+        ->name('car.create');
+
+    Route::post('car/{car}',[CarController::class,'update'])
+        ->name('car.update');
+
+    Route::get('car/{car}',[CarController::class,'show'])
+        ->name('car.show');
+
+    Route::delete('car/{car}',[CarController::class,'destroy'])
+        ->name('car.destroy');
+
+    Route::get('car/{car}/edit',[CarController::class,'edit'])
+        ->name('car.edit')
+        ->can('edit-car','car');
+});
 
 require __DIR__.'/auth.php';
