ccca1f816d5f910992bb216404369b5f20f0901c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-12-19 16:00:44 +0200

    Update

--- config/wireui.php
+++ config/wireui.php
@@ -66,7 +66,7 @@ return [
 
     'dialog' => Config::dialog(),
 
-    'dropdown' => Config::dropdown(),
+    'dropdown' => Config::dropdown(['alias'=> 'wui-dropdown']),
 
     'icon' => Config::icon(),
 
